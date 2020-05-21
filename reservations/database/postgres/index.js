981740caded96b0d26710f0dd6061d06991b974b
Robert Heler <robertheler@gmail.com>
2020-05-21 07:24:47 -0700

    API returns availability for given date and party size

--- reservations/database/postgres/index.js
+++ reservations/database/postgres/index.js
@@ -1,90 +1,141 @@
-const {Pool, Client} = require("pg");
-const moment = require('moment'); // require
+const { Pool, Client } = require("pg");
+const moment = require("moment"); // require
 moment().format();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost', // change to databse for deplying
-  database: 'ratemyrestaurant',
-  password: 'postgres',
+  user: "postgres",
+  host: "localhost", // change to databse for deplying
+  database: "ratemyrestaurant",
+  password: "postgres",
   port: 5432
 });
 
 //GET api/restaurants/:id
-const get = (id, callback) => {
-  pool.query(`SELECT name FROM restaurants WHERE restaurant_id = ${id}`, (err, name) => {
-    if (err) {
-      callback(err);
-    } else if (name.rows.length === 0) {
-      callback(null, {});
-    } else {
-      pool.query(`SELECT * FROM availability INNER JOIN tables ON (availability.table_id = tables.table_id) WHERE tables.restaurant_id = ${id}`, (err, res) => {
-        if (err) {
-          callback(err);
-        } else {
-          let result = {
-            restaurant_id: id,
-            restaurant_name: name.rows[0].name,
-            availability: {}
-          }
-          for (var i = 0; i < res.rows.length; i ++) {
-            let date = moment(res.rows[i].date).format('MM/DD/YYYY');
-            if (!result.availability[date]) {
-              result.availability[date] = [{
-                table_id: res.rows[i].table_id,
-                table_capacity: res.rows[i].table_capacity,
-                available_times: res.rows[i].available_times}]
+const getAllAvailability = (id, callback) => {
+  pool.query(
+    `SELECT name FROM restaurants WHERE restaurant_id = ${id}`,
+    (err, name) => {
+      if (err) {
+        callback(err);
+      } else if (name.rows.length === 0) {
+        callback(null, {});
+      } else {
+        pool.query(
+          `SELECT * FROM availability INNER JOIN tables ON (availability.table_id = tables.table_id) WHERE tables.restaurant_id = ${id}`,
+          (err, res) => {
+            if (err) {
+              callback(err);
             } else {
-              result.availability[date].push({
-                table_id: res.rows[i].table_id,
-                table_capacity: res.rows[i].table_capacity,
-                available_times: res.rows[i].available_times
-              })
+              let result = {
+                restaurant_id: id,
+                restaurant_name: name.rows[0].name,
+                dates: {}
+              };
+              for (var i = 0; i < res.rows.length; i++) {
+                let date = moment(res.rows[i].date).format("MM/DD/YYYY");
+                if (!result.dates[date]) {
+                  result.dates[date] = [
+                    {
+                      table_id: res.rows[i].table_id,
+                      table_capacity: res.rows[i].table_capacity,
+                      available_times: res.rows[i].available_times
+                    }
+                  ];
+                } else {
+                  result.dates[date].push({
+                    table_id: res.rows[i].table_id,
+                    table_capacity: res.rows[i].table_capacity,
+                    available_times: res.rows[i].available_times
+                  });
+                }
+              }
+              callback(err, result);
             }
           }
-          callback(err, result);
-        }
-      })
+        );
+      }
     }
-  });
-  // pool.query(`SELECT * FROM availability INNER JOIN tables ON (availability.table_id = tables.table_id) WHERE tables.restaurant_id = ${id}`, (err, res) => {
-  //   if (err) {
-  //     callback(err);
-  //   } else {
-  //     console.log(res.rows);
-  //     pool.query(`SELECT name FROM restaurants WHERE restaurant_id = ${id}`, (err, name) => {
-  //       if (err) {
-  //         callback(err);
-  //       } else if (name.rows.length === 0){
-  //         callback(null, {});
-  //       } else {
-  //         output = {
-  //           restaurant_id: id,
-
-  //         }
-  //         callback(err, res.rows);
-  //       }
-  //     })
-  //   }
-  // })
+  );
 };
+
+//GET api/restaurants/:id
+const getSpecificAvailability = (id, date, size, callback) => {
+  console.log(date);
+  pool.query(
+    `SELECT name FROM restaurants WHERE restaurant_id = ${id}`,
+    (err, name) => {
+      if (err) {
+        callback(err);
+      } else if (name.rows.length === 0) {
+        callback(null, {});
+      } else {
+        pool.query(
+          `SELECT *
+          FROM availability INNER JOIN tables
+          ON (availability.table_id = tables.table_id)
+          WHERE tables.restaurant_id = ${id}
+          AND tables.table_capacity >= ${size}
+          AND availability.date::date = '${date}'`,
+          (err, res) => {
+            if (err) {
+              callback(err);
+            } else {
+              let result = {
+                id: id,
+                name: name.rows[0].name,
+                date: date,
+                tables: []
+              };
+              for (var i = 0; i < res.rows.length; i++) {
+                  result.tables.push({
+                    id: res.rows[i].table_id,
+                    capacity: res.rows[i].table_capacity,
+                    times: res.rows[i].available_times
+                  });
+              }
+              callback(err, result);
+            }
+          }
+        );
+      }
+    }
+  );
+};
+// pool.query(`SELECT * FROM availability INNER JOIN tables ON (availability.table_id = tables.table_id) WHERE tables.restaurant_id = ${id}`, (err, res) => {
+//   if (err) {
+//     callback(err);
+//   } else {
+//     console.log(res.rows);
+//     pool.query(`SELECT name FROM restaurants WHERE restaurant_id = ${id}`, (err, name) => {
+//       if (err) {
+//         callback(err);
+//       } else if (name.rows.length === 0){
+//         callback(null, {});
+//       } else {
+//         output = {
+//           restaurant_id: id,
+
+//         }
+//         callback(err, res.rows);
+//       }
+//     })
+//   }
+// })
 
 //POST api/restaurants/:name
 const post = (name, callback) => {
-  pool.query(`INSERT INTO restaurants(name) VALUES('${name}')`,
-  (err, res) => {
+  pool.query(`INSERT INTO restaurants(name) VALUES('${name}')`, (err, res) => {
     if (err) {
       callback(err);
     } else {
-      callback(null, res)
+      callback(null, res);
     }
-  })
+  });
 };
 
-module.exports.get= get;
+module.exports.getSpecificAvailability = getSpecificAvailability;
+module.exports.getAllAvailability = getAllAvailability;
 module.exports.post = post;
-
-
 
 // //routes
 // app.post("/api/property/", async (req, res) => {
@@ -98,4 +149,3 @@ module.exports.post = post;
 //     console.error(err.message);
 //   }
 // });
-
