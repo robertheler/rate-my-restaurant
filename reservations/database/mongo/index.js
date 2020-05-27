cf45b0881dd9d6614b266278cef8549f678d91af
Robert Heler <robertheler@gmail.com>
2020-05-26 21:27:54 -0700

    Implemented CRUD for Mongo and benchmarked Postgres vs Mongo

--- reservations/database/mongo/index.js
+++ reservations/database/mongo/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const schema = require('./schema');
+const moment = require("moment"); // require
+moment().format();
 
 let options = {
   useNewUrlParser: true,
@@ -13,41 +15,77 @@ mongoose.connect('mongodb://localhost/restaurants', options);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to database!');
-  //seeder.seed();
-  mongoose.connection.db.dropDatabase();
+  console.log('Connected to Mongo Database!');
 });
 
-function get (id, callback) {
-  schema.Restaurants.findOne({ id }, (err, schedule) => {
-    if (err) {
-      console.log(err);
-      callback(err);
-    } else {
-      callback(null, schedule);
-    }
-  });
+function getRestaurant (id, callback) {
+  schema.Restaurants.find({id: id})
+  //.explain('executionStats')
+  .exec(callback);
 };
 
-// findAll retrieves all shoes
-function findAll(callback) {
-  schema.Restaurants.find({}, callback);
-}
+function postRestaurant (restaurant, callback) {
+  schema.Restaurants.create(restaurant, callback);
+};
 
-// findOne will retrieve the shoe associated with the given id
-function findOne(id, callback) {
-  mongoose.connect('mongodb://mongo:27017/adidas', options, () => {
-    schema.Restaurants.find({id: id}, callback)
+function deleteRestaurant (id, callback) {
+  schema.Restaurants.deleteOne({id:id}, callback);
+};
+
+
+function patchRestaurant (id, changes, callback) {
+  schema.Restaurants.updateOne({id:id},changes, callback);
+};
+
+function getSpecificAvailability (id, date, size, callback) {
+  getRestaurant(id, (err, restaurant) => {
+    if (err) {
+      callback(err)
+    }
+    else if (restaurant.length === 0) {
+      console.log([]);
+      callback(null, [])
+    } else {
+      let tables = restaurant[0].tables;
+      let output = {
+        id: restaurant[0].id,
+        name: restaurant[0].name,
+        date: date,
+        tables: []
+      };
+      // for each table, check available dates if capacity greater than input
+      for (var i = 0; i < tables.length; i++) {
+        if (tables[i].capacity >= size) {
+          let times = [];
+          let dateFound = false;
+          // if input date in database, push its available times to output
+          for (let j = 0; j < tables[i].dates.length; j++) {
+            let currentDate = moment(tables[i].dates[j].date).format("MM-DD-YYYY");
+            if (currentDate ===  date) {
+              times = tables[i].dates[j].times;
+              dateFound = true;
+            }
+          }
+          if (dateFound) {
+            let table = {
+              id: tables[i].id,
+              capacity: tables[i].capacity,
+              times: times
+            };
+            output.tables.push(table)
+          }
+        } else {
+          continue
+        }
+      }
+      callback(null, output);
+    }
   })
-}
-
-// insertOne inserts a shoe into the db
-function insertOne(shoe, callback) {
-  schema.Restaurants.create(shoe, callback);
-}
+};
 
 
-module.exports.get = get;
-module.exports.findOne = findOne;
-module.exports.findAll = findAll;
-module.exports.insertOne = insertOne;
+module.exports.getRestaurant = getRestaurant;
+module.exports.postRestaurant = postRestaurant;
+module.exports.deleteRestaurant = deleteRestaurant;
+module.exports.patchRestaurant = patchRestaurant;
+module.exports.getSpecificAvailability = getSpecificAvailability ;
--- reservations/database/mongo/seed.js
+++ reservations/database/mongo/seed.js
@@ -1,20 +1,10 @@
 const data = require("./data.json");
-var each = require('async/each');
-// const data1 = require("../data/data1.json");
-// const data2 = require("../data/data1.json");
-// const data3 = require("../data/data1.json");
-// const data4 = require("../data/data1.json");
 const mongoose = require('mongoose');
-// /const schema = require('./schema.js');
 const Restaurants = require('./index.js');
 
-
-const db = mongoose.connection;
-
-let query, values;
 let i = 0;
-console.log(data.length);
 for (let restaurant of data) {
+  i++
   console.log(i);i++
   setTimeout(
     () => {
@@ -25,6 +15,6 @@ for (let restaurant of data) {
           console.log(`${restaurant.id} inserted into the collection`);
         }
       })
-    }, 300 * i
+    }, 100 * i
   )
 }
